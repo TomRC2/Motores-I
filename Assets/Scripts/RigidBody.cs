9a97e49957c0c7488b25d840616033edf62cc2c0
TomRC2 <tomyulanosky@gmail.com>
2024-08-26 20:24:58 -0300

    Classwork

--- Assets/Scripts/RigidBody.cs
+++ Assets/Scripts/RigidBody.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class RigidBody : MonoBehaviour
 {
@@ -8,10 +11,15 @@ public class RigidBody : MonoBehaviour
     public Vector2 inputVector;
     public Rigidbody Rigidbody;
     public int Jumpforce;
+    public bool CanJump;
+    public Collision Contacto;
+    public int puntos;
+    public TMPro.TextMeshPro TextoPuntos;
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody = GetComponent<Rigidbody>();
+        CanJump = true;
     }
 
     // Update is called once per frame
@@ -22,9 +30,41 @@ public class RigidBody : MonoBehaviour
 
         Rigidbody.AddForce(inputVector.x * speed, 0f, inputVector.y * speed, ForceMode.Impulse);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        
+        if(Input.GetKeyDown(KeyCode.Space) && CanJump)
         {
             Rigidbody.AddForce(0f, Jumpforce, 0f, ForceMode.Impulse);
+            CanJump = false;
+        }
+
+
+        
+        
+    }
+    private void OnCollisionEnter(Collision Contacto)
+    {
+        Debug.Log("Choque contra: " + Contacto.gameObject.name);
+        if (Contacto.gameObject.CompareTag("Nivel"))
+        {
+            CanJump = true;
+        }
+
+        if (Contacto.gameObject.CompareTag("KillZone"))
+        {
+            SceneManager.LoadScene(0); 
+        }
+
+        if(Contacto.gameObject.CompareTag("Goal"))
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        if(Contacto.gameObject.CompareTag("Item"))
+        {
+            Destroy(Contacto.gameObject);
+            puntos++;
+            TextoPuntos.text = puntos.ToString();
         }
     }
 }
+
