e490491f1d5655bd1cf539528d5401d207e1e98a
TomRC2 <tomyulanosky@gmail.com>
2024-08-26 20:36:53 -0300

    Agregado script menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
@@ -14,5 +15,9 @@ public class Menu : MonoBehaviour
     void Update()
     {
         
+    }
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(0);
     }
 }
