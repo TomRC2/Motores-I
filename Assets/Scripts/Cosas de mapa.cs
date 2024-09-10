d659b991197a9b251dd1b69472b6f962f6417362
TomRC2 <tomyulanosky@gmail.com>
2024-09-09 22:53:43 -0300

    CameraThirdPerson
    Luego de 500 horas finalmente logre hacer la camara.
    Tambien una parte del mapa

--- Assets/Scripts/Cosas de mapa.cs
+++ Assets/Scripts/Cosas de mapa.cs
@@ -7,47 +7,23 @@ using UnityEngine.UI;
 
 public class RigidBody : MonoBehaviour
 {
-    public float speed;
+   
     public Vector2 inputVector;
     public Rigidbody Rigidbody;
-    public int Jumpforce;
-    public bool CanJump;
     public Collision Contacto;
     public int puntos;
     public TMPro.TextMeshPro TextoPuntos;
-    // Start is called before the first frame update
+   
     void Start()
     {
         Rigidbody = GetComponent<Rigidbody>();
-        CanJump = true;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        inputVector.x = Input.GetAxis("Horizontal");
-        inputVector.y = Input.GetAxis("Vertical");
-
-        Rigidbody.AddForce(inputVector.x * speed, 0f, inputVector.y * speed, ForceMode.Impulse);
-
-        
-        if(Input.GetKeyDown(KeyCode.Space) && CanJump)
-        {
-            Rigidbody.AddForce(0f, Jumpforce, 0f, ForceMode.Impulse);
-            CanJump = false;
-        }
-
-
-        
         
     }
+
+    
     private void OnCollisionEnter(Collision Contacto)
     {
-        Debug.Log("Choque contra: " + Contacto.gameObject.name);
-        if (Contacto.gameObject.CompareTag("Nivel"))
-        {
-            CanJump = true;
-        }
+        
 
         if (Contacto.gameObject.CompareTag("KillZone"))
         {
